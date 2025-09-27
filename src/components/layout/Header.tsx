e044f8663b2162b80447244823bd74c731fc85e5
Sehaj <arorasehajbir@gmail.com>
2025-09-27 09:11:26 +0530

    init

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -13,7 +13,7 @@ export function Header() {
           {/* Logo */}
           <div className="flex items-center gap-3">
             <div className="relative">
-              <img src="logoPioneerX.png" className="w-10 h-10" />
+              {/* <img src="'/Users/sbsxngh/Developer/MARK 85/PioneerX/src/components/layout/logoPioneerX.png'" className="w-10 h-10" /> */}
               {/* <div className="w-10 h-10 bg-gradient-hero rounded-xl flex items-center justify-center glow-primary">
                 <Sparkles className="w-6 h-6 text-primary-foreground" />
               </div> */}
